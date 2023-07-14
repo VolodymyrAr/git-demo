f2704701c5693c43efe31493b688b696193246a8
Volodymyr Arestarkhov <v.arestarkhov@smartweb.com.ua>
2023-07-14 10:57:38 +0300

    first commit

---
+++ sample_code.py
@@ -0,0 +1,16 @@
+class User:
+
+    def __init__(self, name):
+        self.name = name
+        self.logged_in = False
+
+    def login(self):
+        self.logged_in = True
+        print(f"{self.name} is logged in.")
+
+    def logout(self):
+        self.logged_in = False
+        print(f"{self.name} is logged out.")
+
+    def __repr__(self):
+        return f"User({self.name})"
